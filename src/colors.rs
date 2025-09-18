46d783975365b5f5f1ae1316566042c15d9d884e
Lukas Kirschner <lukas@lukaskirschner.de>
2025-09-18 22:57:31 +0200

    Implemented Board Drawing

--- src/colors.rs
+++ src/colors.rs
@@ -5,10 +5,10 @@ pub const BOARD_BORDER_COLOR: Color = Color::RGB(255, 255, 255);
 pub const BOARD_BACKGROUND_COLOR: Color = Color::RGB(20, 0, 60);
 pub const SAND_COLOR: Color = Color::RGB(200, 200, 150);
 impl Element {
-    pub fn color(&self) -> &Color {
+    pub fn color(&self) -> Color {
         match self {
-            Element::None => &BOARD_BACKGROUND_COLOR,
-            Element::Sand => &SAND_COLOR,
+            Element::None => BOARD_BACKGROUND_COLOR,
+            Element::Sand => SAND_COLOR,
         }
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -15,14 +15,14 @@ impl Ui {
         Self {
             win_width: width,
             win_height: height,
-            board_width: width - 80,
-            board_height: height - 20,
+            board_width: width - 120,
+            board_height: height - 40,
         }
     }
     /// Draw the window content
     pub fn draw(&self, canvas: &mut WindowCanvas, world: &GameWorld) -> Result<(), String> {
-        let left_padding: i32 = ((self.win_width - self.board_width) / 2) as i32;
-        let top_padding: i32 = ((self.win_height - self.board_height) / 2) as i32;
+        let left_padding: i32 = ((self.win_width - self.board_width) / 2) as i32 - 1;
+        let top_padding: i32 = ((self.win_height - self.board_height) / 2) as i32 - 1;
         let right_padding: i32 = ((self.win_width as i32 - left_padding)
             + ((self.win_width - self.board_width) % 2) as i32);
         let bottom_padding: i32 = ((self.win_height as i32 - top_padding)
@@ -47,6 +47,12 @@ impl Ui {
             Point::from((left_padding, top_padding)),
         )?;
         // Draw the board
+        for y in 0..self.board_height{
+            for x in 0..self.board_width{
+                canvas.set_draw_color(world.board()[x][y].color());
+                canvas.draw_point(Point::from((x as i32 + left_padding + 1,y as i32 + top_padding + 1)))?;
+            }
+        }
 
         // Finalize
         canvas.present();
