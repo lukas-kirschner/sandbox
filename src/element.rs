8a87c9fed62cad16a3603cfee218899fe8879a71
Lukas Kirschner <lukas@lukaskirschner.de>
2025-09-23 19:18:06 +0200

    Fixed wrong prev_i causing sand to disappear

--- src/element.rs
+++ src/element.rs
@@ -1,4 +1,4 @@
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Element {
     None,
     Sand,
--- src/world.rs
+++ src/world.rs
@@ -1,7 +1,7 @@
 use crate::element::Element;
 use crate::ui::Ui;
 use rand::{Rng, RngCore};
-use std::cmp::{max, min, Ordering};
+use std::cmp::{Ordering, max, min};
 
 enum Move {
     MoveElement {
@@ -15,21 +15,19 @@ impl Move {
     pub fn same_dest_as(&self, other: &Move) -> bool {
         match self {
             Move::MoveElement {
-                from_x,
-                from_y,
+                from_x: _,
+                from_y: _,
                 to_x,
                 to_y,
             } => {
                 let (my_x, my_y) = (to_x, to_y);
                 match other {
                     Move::MoveElement {
-                        from_x,
-                        from_y,
+                        from_x: _,
+                        from_y: _,
                         to_x,
                         to_y,
-                    } => {
-                        to_x == my_x && to_y == my_y
-                    },
+                    } => to_x == my_x && to_y == my_y,
                 }
             },
         }
@@ -55,7 +53,7 @@ impl GameWorld {
         }
     }
     /// Try to push a 'move down' to the moves vector and return true if that succeeded.
-    fn move_down(&mut self, x: usize, y: usize, rng: &mut dyn RngCore) -> bool {
+    fn move_down(&mut self, x: usize, y: usize, _rng: &mut dyn RngCore) -> bool {
         if y < (self.board[0].len() - 1) {
             if self.board[x][y + 1] == Element::None {
                 self.moves.push(Move::MoveElement {
@@ -101,11 +99,11 @@ impl GameWorld {
         false
     }
     /// Tick (Calculate the next iteration of this board in-place)
-    pub fn tick(&mut self, rng: &mut dyn RngCore) -> () {
+    pub fn tick(&mut self, rng: &mut dyn RngCore) {
         self.moves.clear();
         let height = self.board[0].len();
         let width = self.board.len();
-        for y in (0..height).rev() {
+        for y in 0..height {
             for x in 0..width {
                 // new_board[x][y] = self.board[x][y];
                 // Gravity
@@ -121,34 +119,43 @@ impl GameWorld {
         // Thanks, https://winter.dev/articles/falling-sand , for this algorithm
 
         // Remove all filled cells from possible moves
-        for mut i in 0..self.moves.len() {
-            let Move::MoveElement {
-                from_x,
-                from_y,
-                to_x,
-                to_y,
-            } = self.moves[i];
-            if self.board[to_x][to_y] != Element::None {
-                self.moves[i] = self.moves.pop().unwrap();
-                i -= 1;
-            }
-        }
+        // let mut i = 0;
+        // while i < self.moves.len() {
+        //     let Move::MoveElement {
+        //         from_x,
+        //         from_y,
+        //         to_x,
+        //         to_y,
+        //     } = self.moves[i];
+        //     debug_assert_ne!(self.board[from_x][from_y], Element::None);
+        //     if self.board[to_x][to_y] != Element::None {
+        //         self.moves[i] = self.moves.pop().unwrap();
+        //     } else {
+        //         i += 1;
+        //     }
+        // }
 
         // Sort moves by destination
-        self.moves.sort_unstable_by(|m1,m2| {
-            match m1{
-                Move::MoveElement { from_x, from_y, to_x, to_y } => {
-                    let (x1,y1) = (to_x,to_y);
-                    match m2{
-                        Move::MoveElement { from_x, from_y, to_x, to_y } => {
-                            match to_x.cmp(x1){
-                                Ordering::Equal => to_y.cmp(y1),
-                                x => x
-                            }
-                        }
-                    }
+        self.moves.sort_unstable_by(|m1, m2| match m1 {
+            Move::MoveElement {
+                from_x: _,
+                from_y: _,
+                to_x,
+                to_y,
+            } => {
+                let (x1, y1) = (to_x, to_y);
+                match m2 {
+                    Move::MoveElement {
+                        from_x: _,
+                        from_y: _,
+                        to_x,
+                        to_y,
+                    } => match to_x.cmp(x1) {
+                        Ordering::Equal => to_y.cmp(y1),
+                        x => x,
+                    },
                 }
-            }
+            },
         });
 
         // Commit moves. If multiple moves into one single destination are possible, select a random one
@@ -161,7 +168,7 @@ impl GameWorld {
         let mut prev_i = 0usize;
         for i in 0..(self.moves.len() - 1) {
             if !self.moves[i].same_dest_as(&self.moves[i + 1]) {
-                let random_choice = rng.random_range((i - prev_i)..=i);
+                let random_choice = rng.random_range(prev_i..=i);
                 // Execute the randomly chosen move
                 match self.moves[random_choice] {
                     Move::MoveElement {
@@ -170,9 +177,10 @@ impl GameWorld {
                         to_x,
                         to_y,
                     } => {
-                        let elem = self.board[from_x][from_y];
-                        self.board[from_x][from_y] = self.board[to_x][to_y];
-                        self.board[to_x][to_y] = elem;
+                        debug_assert_ne!(self.board[from_x][from_y], Element::None);
+                        debug_assert_eq!(self.board[to_x][to_y], Element::None);
+                        self.board[to_x][to_y] = self.board[from_x][from_y];
+                        self.board[from_x][from_y] = Element::None;
                     },
                 }
                 prev_i = i + 1;
@@ -190,5 +198,34 @@ impl GameWorld {
     }
     pub fn board(&self) -> &Vec<Vec<Element>> {
         &self.board
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::element::Element;
+    use crate::world::GameWorld;
+    use rand::SeedableRng;
+    use rand_xorshift::XorShiftRng;
+
+    #[test]
+    fn test_fall_tick_simple() {
+        let mut board = GameWorld::new(3, 3);
+        let mut rng = XorShiftRng::seed_from_u64(0);
+        board.board[1][1] = Element::Sand;
+        board.tick(&mut rng);
+        assert_eq!(board.board[1][1], Element::None);
+        assert_eq!(board.board[1][2], Element::Sand);
+    }
+    #[test]
+    fn test_fall_tick_stacked() {
+        let mut board = GameWorld::new(3, 3);
+        let mut rng = XorShiftRng::seed_from_u64(0);
+        board.board[1][0] = Element::Sand;
+        board.board[1][1] = Element::Sand;
+        board.tick(&mut rng);
+        assert_eq!(board.board[1][0], Element::None);
+        assert_eq!(board.board[1][1], Element::None);
+        assert_eq!(board.board[1][2], Element::Sand);
     }
 }
